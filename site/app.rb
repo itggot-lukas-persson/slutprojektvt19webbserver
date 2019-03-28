44e07ce7164b59af8e088c21a2cb22ab0eef42ca
itggot-lukas-persson <lukas.persson@itggot.se>
2019-03-28 10:57:20 +0100

    kan se om man är inloggad eller inte,

--- site/app.rb
+++ site/app.rb
@@ -5,7 +5,13 @@ require 'bcrypt'
 enable :sessions
 
 get('/') do
-    slim(:index)
+    if session[:username]
+        db = SQLite3::Database.new("db/forum.db")
+        current_user = db.execute("SELECT username, password FROM users WHERE username = ?",session[:username]).first
+    else 
+        current_user = nil
+    end
+    slim(:index, locals:{current_user:current_user})
 end
 
 get('/login') do
@@ -21,7 +27,8 @@ post('/newlogin') do
     db.results_as_hash = true
     result = db.execute("SELECT username, password FROM users WHERE username = ?",params[:username]).first
     if BCrypt::Password.new(result["password"]) == params[:password]
-        redirect('/logged')
+        session[:username] = result["username"]
+        redirect('/')
     else
         redirect('/error')
     end
@@ -37,4 +44,15 @@ post('/register/new') do
     hash_passsword = BCrypt::Password.create(params[:password])
     db.execute('INSERT INTO users(username, email, password) VALUES ((?), (?), (?))', params[:username], params[:email], hash_passsword)
     redirect('/') 
+end
+
+
+get('/newpost') do 
+    if !session[:username] #Kollar att man är inloggad innan man får komma in
+        redirect back
+    end
+
+
+
+
 end
